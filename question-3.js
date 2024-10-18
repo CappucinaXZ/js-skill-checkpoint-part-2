d5c8a1171437a97bbd0be0a0cc133b4afd1bcaac
Mocca <p0n-@hotmail.com>
2024-10-18 14:09:22 +0700

    Mocca SkillCheck2

--- question-3.js
+++ question-3.js
@@ -1,14 +1,13 @@
 // เริ่มเขียนโค้ดตรงนี้
 let getUsers = async() => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    //const data = await new Promise(resolve => setTimeout(() => resolve(response.json()), 2500));
     const data = response.json();
     return data
 }
 async function asynchronousFunction() {
     const users = await getUsers();
     console.log(users)
-    console.log(users[0].name.length)
+    //console.log(users[0].name.length)
     let result = users.filter(result => result.name.length>17).map(result => result.name)
     console.log(result)
 
